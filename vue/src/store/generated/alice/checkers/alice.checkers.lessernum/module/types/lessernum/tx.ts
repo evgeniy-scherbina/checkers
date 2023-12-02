d4b14b284e4d724e11399121c757f5029f06ad5d
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2023-12-02 15:36:15 -0500

    Added CreateGame API

--- vue/src/store/generated/alice/checkers/alice.checkers.lessernum/module/types/lessernum/tx.ts
+++ vue/src/store/generated/alice/checkers/alice.checkers.lessernum/module/types/lessernum/tx.ts
@@ -1,13 +1,164 @@
 /* eslint-disable */
+import { Reader, Writer } from "protobufjs/minimal";
+
 export const protobufPackage = "alice.checkers.lessernum";
 
+export interface MsgCreateGame {
+  creator: string;
+  player1: string;
+  player2: string;
+}
+
+export interface MsgCreateGameResponse {}
+
+const baseMsgCreateGame: object = { creator: "", player1: "", player2: "" };
+
+export const MsgCreateGame = {
+  encode(message: MsgCreateGame, writer: Writer = Writer.create()): Writer {
+    if (message.creator !== "") {
+      writer.uint32(10).string(message.creator);
+    }
+    if (message.player1 !== "") {
+      writer.uint32(18).string(message.player1);
+    }
+    if (message.player2 !== "") {
+      writer.uint32(26).string(message.player2);
+    }
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): MsgCreateGame {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgCreateGame } as MsgCreateGame;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        case 1:
+          message.creator = reader.string();
+          break;
+        case 2:
+          message.player1 = reader.string();
+          break;
+        case 3:
+          message.player2 = reader.string();
+          break;
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(object: any): MsgCreateGame {
+    const message = { ...baseMsgCreateGame } as MsgCreateGame;
+    if (object.creator !== undefined && object.creator !== null) {
+      message.creator = String(object.creator);
+    } else {
+      message.creator = "";
+    }
+    if (object.player1 !== undefined && object.player1 !== null) {
+      message.player1 = String(object.player1);
+    } else {
+      message.player1 = "";
+    }
+    if (object.player2 !== undefined && object.player2 !== null) {
+      message.player2 = String(object.player2);
+    } else {
+      message.player2 = "";
+    }
+    return message;
+  },
+
+  toJSON(message: MsgCreateGame): unknown {
+    const obj: any = {};
+    message.creator !== undefined && (obj.creator = message.creator);
+    message.player1 !== undefined && (obj.player1 = message.player1);
+    message.player2 !== undefined && (obj.player2 = message.player2);
+    return obj;
+  },
+
+  fromPartial(object: DeepPartial<MsgCreateGame>): MsgCreateGame {
+    const message = { ...baseMsgCreateGame } as MsgCreateGame;
+    if (object.creator !== undefined && object.creator !== null) {
+      message.creator = object.creator;
+    } else {
+      message.creator = "";
+    }
+    if (object.player1 !== undefined && object.player1 !== null) {
+      message.player1 = object.player1;
+    } else {
+      message.player1 = "";
+    }
+    if (object.player2 !== undefined && object.player2 !== null) {
+      message.player2 = object.player2;
+    } else {
+      message.player2 = "";
+    }
+    return message;
+  },
+};
+
+const baseMsgCreateGameResponse: object = {};
+
+export const MsgCreateGameResponse = {
+  encode(_: MsgCreateGameResponse, writer: Writer = Writer.create()): Writer {
+    return writer;
+  },
+
+  decode(input: Reader | Uint8Array, length?: number): MsgCreateGameResponse {
+    const reader = input instanceof Uint8Array ? new Reader(input) : input;
+    let end = length === undefined ? reader.len : reader.pos + length;
+    const message = { ...baseMsgCreateGameResponse } as MsgCreateGameResponse;
+    while (reader.pos < end) {
+      const tag = reader.uint32();
+      switch (tag >>> 3) {
+        default:
+          reader.skipType(tag & 7);
+          break;
+      }
+    }
+    return message;
+  },
+
+  fromJSON(_: any): MsgCreateGameResponse {
+    const message = { ...baseMsgCreateGameResponse } as MsgCreateGameResponse;
+    return message;
+  },
+
+  toJSON(_: MsgCreateGameResponse): unknown {
+    const obj: any = {};
+    return obj;
+  },
+
+  fromPartial(_: DeepPartial<MsgCreateGameResponse>): MsgCreateGameResponse {
+    const message = { ...baseMsgCreateGameResponse } as MsgCreateGameResponse;
+    return message;
+  },
+};
+
 /** Msg defines the Msg service. */
-export interface Msg {}
+export interface Msg {
+  /** this line is used by starport scaffolding # proto/tx/rpc */
+  CreateGame(request: MsgCreateGame): Promise<MsgCreateGameResponse>;
+}
 
 export class MsgClientImpl implements Msg {
   private readonly rpc: Rpc;
   constructor(rpc: Rpc) {
     this.rpc = rpc;
+  }
+  CreateGame(request: MsgCreateGame): Promise<MsgCreateGameResponse> {
+    const data = MsgCreateGame.encode(request).finish();
+    const promise = this.rpc.request(
+      "alice.checkers.lessernum.Msg",
+      "CreateGame",
+      data
+    );
+    return promise.then((data) =>
+      MsgCreateGameResponse.decode(new Reader(data))
+    );
   }
 }
 
@@ -18,3 +169,14 @@ interface Rpc {
     data: Uint8Array
   ): Promise<Uint8Array>;
 }
+
+type Builtin = Date | Function | Uint8Array | string | number | undefined;
+export type DeepPartial<T> = T extends Builtin
+  ? T
+  : T extends Array<infer U>
+  ? Array<DeepPartial<U>>
+  : T extends ReadonlyArray<infer U>
+  ? ReadonlyArray<DeepPartial<U>>
+  : T extends {}
+  ? { [K in keyof T]?: DeepPartial<T[K]> }
+  : Partial<T>;
---
+++ x/lessernum/keeper/msg_server_create_game.go
@@ -0,0 +1,36 @@
+package keeper
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/alice/checkers/x/lessernum/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func (k msgServer) CreateGame(goCtx context.Context, msg *types.MsgCreateGame) (*types.MsgCreateGameResponse, error) {
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	// TODO: Handling the message
+	_ = ctx
+
+	systemInfo, found := k.GetSystemInfo(ctx)
+	if !found {
+		panic("can't get system info")
+	}
+	nextId := strconv.FormatUint(systemInfo.NextId, 10)
+
+	game := types.StoredGame{
+		Index:   nextId,
+		Player1: msg.Player1,
+		Player2: msg.Player2,
+	}
+	k.SetStoredGame(ctx, game)
+
+	systemInfo.NextId++
+	k.SetSystemInfo(ctx, systemInfo)
+
+	return &types.MsgCreateGameResponse{
+		GameId: nextId,
+	}, nil
+}
