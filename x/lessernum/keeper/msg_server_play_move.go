fa538c4ef20c975bbb31fea94aaba8842d25d7ef
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2023-12-02 18:31:00 -0500

    Tests are passing

--- x/lessernum/keeper/msg_server_play_move.go
+++ x/lessernum/keeper/msg_server_play_move.go
@@ -2,7 +2,7 @@ package keeper
 
 import (
 	"context"
-
+	"fmt"
 	"github.com/alice/checkers/x/lessernum/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -18,11 +18,48 @@ func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*typ
 		return nil, types.ErrGameNotFound
 	}
 
+	active := isGameActive(&storedGame)
+	if !active {
+		return nil, fmt.Errorf("game is not active")
+	}
+
+	ok := isPlayerTurn(&storedGame, msg.Creator)
+	if !ok {
+		return nil, fmt.Errorf("not your turn")
+	}
+
+	makeMove(&storedGame, msg.Number)
+	k.SetStoredGame(ctx, storedGame)
+
 	return &types.MsgPlayMoveResponse{}, nil
 }
 
-func isPlayerTurn(storedGame *types.StoredGame, player string) bool {
-	switch storedGame. {
+func isGameActive(storedGame *types.StoredGame) bool {
+	return storedGame.PlayerToMove == 1 || storedGame.PlayerToMove == 2
+}
 
+func isPlayerTurn(storedGame *types.StoredGame, player string) bool {
+	switch storedGame.PlayerToMove {
+	case 1:
+		return storedGame.Player1 == player
+	case 2:
+		return storedGame.Player2 == player
+	default:
+		return false
+	}
+}
+
+func makeMove(storedGame *types.StoredGame, number uint64) {
+	switch storedGame.PlayerToMove {
+	case 1:
+		storedGame.Move1 = number
+	case 2:
+		storedGame.Move2 = number
+
+		if storedGame.Move1 < storedGame.Move2 {
+			storedGame.Winner = storedGame.Player1
+		} else {
+			storedGame.Winner = storedGame.Player2
+		}
 	}
 }
