b060056bf96c066b5cb869df17696e919a42c6a7
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-01-23 11:48:57 -0500

    Play move placeholder

--- x/oddnum/keeper/msg_server_play_move.go
+++ x/oddnum/keeper/msg_server_play_move.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/alice/checkers/x/oddnum/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -12,6 +13,12 @@ func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*typ
 
 	// TODO: Handling the message
 	_ = ctx
+
+	storedGame, found := k.Keeper.GetStoredGame(ctx, msg.GameIndex)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "%s", msg.GameIndex)
+	}
+	_ = storedGame
 
 	return &types.MsgPlayMoveResponse{}, nil
 }
--- x/oddnum/types/errors.go
+++ x/oddnum/types/errors.go
@@ -10,4 +10,8 @@ import (
 var (
 	ErrInvalidBlack = sdkerrors.Register(ModuleName, 1100, "black address is invalid: %s")
 	ErrInvalidRed   = sdkerrors.Register(ModuleName, 1101, "red address is invalid: %s")
+
+	ErrGameNotFound     = sdkerrors.Register(ModuleName, 1106, "game by id not found")
+	ErrCreatorNotPlayer = sdkerrors.Register(ModuleName, 1107, "message creator is not a player")
+	ErrNotPlayerTurn    = sdkerrors.Register(ModuleName, 1108, "player tried to play out of turn")
 )
