8c3a4e13383bb9c0718f0f136f15c2acdf35f780
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-01-23 13:10:02 -0500

    Update playMove

--- x/oddnum/keeper/msg_server_play_move.go
+++ x/oddnum/keeper/msg_server_play_move.go
@@ -2,10 +2,10 @@ package keeper
 
 import (
 	"context"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 	"github.com/alice/checkers/x/oddnum/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*types.MsgPlayMoveResponse, error) {
@@ -19,6 +19,15 @@ func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*typ
 		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "%s", msg.GameIndex)
 	}
 	_ = storedGame
+
+	black, err := storedGame.GetBlackAddress()
+	if err != nil {
+		panic(err.Error())
+	}
+	err = k.bank.SendCoinsFromAccountToModule(ctx, black, types.ModuleName, sdk.NewCoins(storedGame.GetWagerCoin()))
+	if err != nil {
+		return nil, sdkerrors.Wrapf(err, types.ErrBlackCannotPay.Error())
+	}
 
 	return &types.MsgPlayMoveResponse{}, nil
 }
