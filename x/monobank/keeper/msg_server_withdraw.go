8e30aa2ba9d7635e958116d320cbf4380367fbfc
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2024-02-05 14:38:07 -0500

    Tests are passing

--- x/monobank/keeper/msg_server_withdraw.go
+++ x/monobank/keeper/msg_server_withdraw.go
@@ -2,7 +2,8 @@ package keeper
 
 import (
 	"context"
-
+	"errors"
+	"github.com/alice/checkers/x/monobank/testutil"
 	"github.com/alice/checkers/x/monobank/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -12,6 +13,23 @@ func (k msgServer) Withdraw(goCtx context.Context, msg *types.MsgWithdraw) (*typ
 
 	// TODO: Handling the message
 	_ = ctx
+
+	withdrawer, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+	balance, found := k.GetBalance(ctx, withdrawer.String())
+	if !found || balance.Balance < msg.Amount {
+		return nil, errors.New("insufficient funds")
+	}
+
+	balance.Balance -= msg.Amount
+	k.SetBalance(ctx, balance)
+
+	err = k.bank.SendCoinsFromModuleToAccount(ctx, types.ModuleName, withdrawer, testutil.CoinsOf(msg.Amount))
+	if err != nil {
+		return nil, err
+	}
 
 	return &types.MsgWithdrawResponse{}, nil
 }
